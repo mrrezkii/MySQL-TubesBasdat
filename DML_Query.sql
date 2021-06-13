e2267f8faae1b710bdc933817b3a96ee33401fb7
nando <muhammad.rezki.ananda@gmail.com>
2021-06-13 18:47:53 +0700

    Insert Actor (Without Foreign Key)

---
+++ DML_Query.sql
@@ -0,0 +1,40 @@
+-- Insert Actor (Without Foreign Key)
+INSERT INTO PPIC
+VALUES("PPIC001", "123abc123", "Muhammad"),
+("PPIC002", "qwerty12345", "Rezki"),
+("PPIC003", "123yeey23", "Ananda");
+
+INSERT INTO Bagian_Pembelian
+VALUES("PEMB001", "123abc123", "Dicky"),
+("PEMB002", "qwerty12345", "Adriansyah"),
+("PEMB003", "123yeey23", "DA");
+
+INSERT INTO Bagian_Gudang
+VALUES("GUDA001", "123abc123", "Anindya"),
+("GUDA002", "qwerty12345", "Prameswari"),
+("UANG003",  "123yeey23", "Putri");
+
+INSERT INTO SPGSPB
+VALUES("PGPB001", "123abc123", "Muhammad"),
+("PGPB002", "qwerty12345", "Rezki"),
+("PPIC003", "123yeey23", "Ananda");
+
+INSERT INTO Sales
+VALUES("SALE001", "123abc123", "Dicky"),
+("SALE002", "qwerty12345", "Adriansyah"),
+("PEMB003", "123yeey23", "DA");
+
+INSERT INTO Bagian_Keuangan
+VALUES("UANG001", "123abc123", "Anindya"),
+("UANG002", "qwerty12345", "Prameswari"),
+("UANG003",  "123yeey23", "Putri");
+
+INSERT INTO Bagian_Kasir
+VALUES("KASI001", "123abc123", "Muhammad"),
+("KASI002", "qwerty12345", "Rezki"),
+("PPIC003", "123yeey23", "Ananda");
+
+INSERT INTO Manager_DCMDS
+VALUES("MANA001", "123abc123", "Dicky"),
+("MANA002", "qwerty12345", "Adriansyah"),
+("PEMB003", "123yeey23", "DA");

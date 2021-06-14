5b01b6ba2d09bd0a9b8480bd27a1a90906aee72b
nando <muhammad.rezki.ananda@gmail.com>
2021-06-14 08:57:01 +0700

    Bagian Gudang - Update Stok

--- UI_DML_Query.sql
+++ UI_DML_Query.sql
@@ -60,3 +60,12 @@ ON bb.username_ppic = PPIC.username;
 UPDATE Bahan_Baku
 SET nama_bahan_baku = "Mantapp", kategori_bahan_baku = "Bahan Baku Belum Jadi", jumlah_stok = 100
 WHERE id_bahan_baku = "BB0001";
+
+-- Bagian Gudang - Update Stok
+SELECT pp.id_purchase_order, pp.id_supplier, pp.id_bahan_baku, pp.tanggal_surat, pp.material_desctiption, pp.warna, pp.unit, pp.qty, pp.unit_price, pp.total_harga, pem.nama_staff_pembelian
+FROM Purchase_Order pp JOIN Bagian_Pembelian pem
+ON pp.username_staff_pembelian = pem.username;
+
+UPDATE Purchase_Order
+SET qty = 10
+WHERE id_purchase_order = "PO0001";

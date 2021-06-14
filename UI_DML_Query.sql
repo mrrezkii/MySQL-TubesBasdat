bfebda3c0e8fff13f2d328a18228147aad9c0ba4
nando <muhammad.rezki.ananda@gmail.com>
2021-06-14 08:51:19 +0700

    Login

---
+++ UI_DML_Query.sql
@@ -0,0 +1,24 @@
+-- Login
+SELECT username, password FROM PPIC
+WHERE username = "PPIC001";
+
+SELECT username, password FROM Bagian_Pembelian
+WHERE username = "PEMB001";
+
+SELECT username, password FROM Bagian_Gudang
+WHERE username = "GUDA001";
+
+SELECT username, password FROM SPGSPB
+WHERE username = "PGPB001";
+
+SELECT username, password FROM Sales
+WHERE username = "SALE001";
+
+SELECT username, password FROM Bagian_Keuangan
+WHERE username = "UANG001";
+
+SELECT username, password FROM Bagian_Kasir
+WHERE username = "KASI001";
+
+SELECT username, password FROM Manager_DCMDS
+WHERE username = "DCMD001";

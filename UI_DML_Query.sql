20331c6f051fb4395d0b534742909d4b862b98cb
nando <muhammad.rezki.ananda@gmail.com>
2021-06-14 09:21:56 +0700

    Bagian Keuangan - Laporan Penjualan

--- UI_DML_Query.sql
+++ UI_DML_Query.sql
@@ -60,6 +60,8 @@ UPDATE Bahan_Baku
 SET nama_bahan_baku = "Mantapp", kategori_bahan_baku = "Bahan Baku Belum Jadi", jumlah_stok = 100
 WHERE id_bahan_baku = "BB0001";
 
+DELETE FROM Bahan_Baku WHERE id_bahan_baku = "BB0009";
+
 -- Bagian Gudang - Update Stok
 SELECT pp.id_purchase_order, pp.id_supplier, pp.id_bahan_baku, pp.tanggal_surat, pp.material_desctiption, pp.warna, pp.unit, pp.qty, pp.unit_price, pp.total_harga, pem.nama_staff_pembelian
 FROM Purchase_Order pp JOIN Bagian_Pembelian pem
@@ -76,3 +78,17 @@ VALUES("SU006", "DO001", "TR0004", "Abdul", "Matahari Kediri", "GUDA001");
 SELECT sj.no_surat_jalan, sj.no_do, sj.no_transport, sj.nama_pemilik, sj.nama_counter, gud.nama_staff_gudang
 FROM Surat_Jalan sj JOIN Bagian_Gudang gud
 ON sj.username_staff_gudang = gud.username;
+
+-- Bagian Keuangan - Laporan Penjualan
+INSERT INTO Laporan_Penjualan
+VALUES("LP008", "SO001", "Papar", "Counter", "UANG001");
+
+SELECT lp.id_laporan_penjualan, lp.id_sales_order, lp.lokasi, lp.jenis_cabang, keu.nama_staff_keuangan
+FROM Laporan_Penjualan lp JOIN Bagian_Keuangan keu
+ON lp.username_staff_keuangan = keu.username;
+
+UPDATE Laporan_Penjualan
+SET lokasi = "Kediri", jenis_cabang = "Counterr"
+WHERE id_laporan_penjualan = "LP001";
+
+DELETE FROM Laporan_Penjualan WHERE id_laporan_penjualan = "LP008";
